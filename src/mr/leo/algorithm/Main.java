05d71dd9ebcd25581d5f45c121cbcc8e9fa74c60
mrleo <mrleo@192.168.0.16>
2017-02-28 21:48:43 -0800

    This is from Leo #1

--- src/mr/leo/algorithm/Main.java
+++ src/mr/leo/algorithm/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world.");
+		test();
 	}
 
+	public static void test() {
+		System.out.println("Hello Mr. Leo.");
+	}
 }
