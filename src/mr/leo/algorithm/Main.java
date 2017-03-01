b3a19a1597c554915fbc41826f1badec0ec079a6
mrleo <mrleo@192.168.0.16>
2017-02-28 22:18:01 -0800

    Problem Three on 2/28/2017 by Leo.

--- src/mr/leo/algorithm/Main.java
+++ src/mr/leo/algorithm/Main.java
@@ -2,13 +2,24 @@ package mr.leo.algorithm;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world.");
-		test();
-	}
-
-	public static void test() {
-		System.out.println("Hello Mr. Leo.");
+	public static void main(String[] args)
+	{
+		Integer [] randomArray = {1, 0, 3, 12, 0, 0, 1, 5, 9};
+		for(int i = 0; i < randomArray.length; i++)
+		{
+			if(randomArray[i] == 0)
+			{
+				for(int j = i; j > 0; j--)	
+				{
+					int randomNumber = randomArray[j];
+					randomArray[j] = randomArray[j - 1];
+					randomArray[j - 1] = randomNumber;
+				}
+			}
+		}
+		for(int i = 0; i < randomArray.length; i++)
+		{
+			System.out.print(randomArray[i] + " ");
+		}
 	}
 }
